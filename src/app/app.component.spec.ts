c115d4016465817cfe858517337df0e85e09ab2c
Michael Ward <michalward82@gmail.com>
2021-06-13 21:28:16 +0100

    add bootstrap and improve styling

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-tour-of-heros'`, () => {
+  it(`should have as title 'mars'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-tour-of-heros');
+    expect(app.title).toEqual('mars');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-tour-of-heros app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('mars app is running!');
   });
 });
